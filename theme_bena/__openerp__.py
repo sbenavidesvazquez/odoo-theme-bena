6cf7374f7bd62022184031680d66535097ddcf4e
samsung <sbenavidesvazquez@danielcastelao.org>
2016-02-24 09:47:43 +0100

    inicial

---
+++ theme_bena/__openerp__.py
@@ -0,0 +1,21 @@
+#Informacion de la pagina
+
+{
+#nombre del tema
+'name': 'Bena tema',
+#Descripcion
+'description' : 'La descripcion de mi tema',
+#version
+'version':'1.0',
+#author
+'author': 'Sergio Benavides',
+#Archivos en uso
+'data':['Views/layout.xml',
+'Views/page.xml',
+'Views/assets.xml'
+],
+#categoria
+'category':'theme/creative',
+#dependencias
+'depends':['website','website_blog','sale']
+}
